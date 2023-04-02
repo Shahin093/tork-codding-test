7b441dec367442a3dfe34dc58f4c29f0106901c8
Shidul Islam <sishahin093@gmail.com>
2023-04-02 10:19:20 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// const prevBtn = document.querySelectorAll('.btn - prv');
+// const nextBtn = document.querySelectorAll((".btn-nextBtn"));
+// const progress = document.getElementById('#progress');
+// const formSteps = document.querySelectorAll(".form-step");
+
+
+// let formStepNum = 0;
+// nextBtn.forEach(btn => {
+//     btn.addEventListener('click', () => {
+//         formStepNum++;
+//         updateFormSteps();
+
+//     })
+// })
+
+// function updateFormSteps() {
+//     formSteps[formStepNum].classList.add('active');
+// }
